0815e195e900fe0df9de5f0c447818ffb945df54
Meandering-Ninja <161959791+Meandering-Ninja@users.noreply.github.com>
2024-07-11 13:53:52 +0530

    Updating Instructions to  Callback.js

--- Callback.js
+++ Callback.js
@@ -13,9 +13,9 @@ div= (a,b) =>{
 }
 function calculator (a,b, callbackfunction1,callbackfunction2,callbackfunction3,callbackfunction4)
 {
-    callbackfunction1(a,b);
-    callbackfunction2(a,b);
-    callbackfunction3(a,b);
-    callbackfunction4(a,b);
+    callbackfunction1(a,b);                           //For Addition
+    callbackfunction2(a,b);                           //For Substaction
+    callbackfunction3(a,b);                           //For Multiplication
+    callbackfunction4(a,b);                           //For Division
 }
-calculator(4,2,sum,sub,mul,div);
+calculator(4,2,sum,sub,mul,div);                    //Passing functions as Arguments . You can pass the numbers as you wish
